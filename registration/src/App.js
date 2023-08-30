375f6f1fd9207c77accc6640376e787aa1ff5e45
mutuku1998 <martinmutuku1998@gmail.com>
2023-08-30 23:11:32 +0300

    creating components

---
+++ registration/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Forminput from './components/Forminput';
+
+function App() {
+  return (
+    <div className="app">
+     <form>
+       <Forminput/>
+     </form>
+    </div>
+  );
+}
+
+export default App;
